786c729efd7c12883568cba6eba1e5da54133097
liujianfeng <992951674@qq.com>
2024-12-05 17:30:49 +0800

    Updates

--- assets/401-C5LOt76u.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as r,a as s,o}from"./index-BDd4kXsD.js";const c={},n={class:"flex items-center justify-center h-full w-full bg-gray-400"};function a(l,e){return o(),r("div",n,e[0]||(e[0]=[s("h2",null,"你没有该页面的权限，请联系管理员",-1)]))}const i=t(c,[["render",a]]);export{i as default};
---
+++ assets/user-BvDm0Shp.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,d as c}from"./index-DLaLWGBO.js";const n={};function o(s,t){return r(),c("div",null,"user 角色可见")}const a=e(n,[["render",o]]);export{a as default};
